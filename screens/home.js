04bb003a545be832b4a0fd68fa6e17637d8b2c51
= <=>
2023-02-05 07:31:41 +0000

    Beautified the home page

--- screens/home.js
+++ screens/home.js
@@ -9,7 +9,8 @@ const textButtons = StyleSheet.create({
     fontSize: 30,
     color: "#fff",
     textAlign:'center',
-    fontFamily: 'Jost_500Medium'
+    fontFamily: 'Jost_500Medium',
+    margin: 60,
   },
 })
 
@@ -17,13 +18,22 @@ const styles = StyleSheet.create({
   container: {
       flex: 1,
       justifyContent: "space-between",
+      alignItems: "center",
       backgroundColor: "#fff",
-      padding: 20,
+      padding: 10,
       margin: 10,
     },
+    toptop: {
+      flex: 0.1,
+      borderTopLeftRadius: 20,
+      borderTopRightRadius: 20,
+      borderBottomLeftRadius: 20,
+      borderBottomRightRadius: 20,
+      justifyContent: 'center', //Centered horizontally
+      alignItems: 'center', //Centered vertically
+    },
     top: {
-      flex: 0.25,
-      backgroundColor: "#00CC66",
+      flex: 0.4,
       borderTopLeftRadius: 20,
       borderTopRightRadius: 20,
       borderBottomLeftRadius: 20,
@@ -32,8 +42,7 @@ const styles = StyleSheet.create({
       alignItems: 'center', //Centered vertically
     },
     middle: {
-      flex: 0.5,
-      backgroundColor: "#00CC66",
+      flex: 0.2,
       borderTopLeftRadius: 20,
       borderTopRightRadius: 20,
       borderBottomLeftRadius: 20,
@@ -42,32 +51,62 @@ const styles = StyleSheet.create({
       alignItems: 'center', //Centered vertically
     },
     bottom: {
-      flex: 0.15,
+      flex: 0.25,
       backgroundColor: "#00CC66",
       borderTopLeftRadius: 20,
       borderTopRightRadius: 20,
       borderBottomLeftRadius: 20,
       borderBottomRightRadius: 20,
+      margin: 10,
       justifyContent: 'center', //Centered horizontally
       alignItems: 'center', //Centered vertically
     },
-    image_style: {height: 250, width: 250, overflow: 'hidden', margin:5},
+    image_style: {height: 105, width: 120, overflow: 'hidden', margin:10},
   });
 
 
 const Home = ({navigation}) => {
 
     return (
+    
+        <View style={styles.container}>
 
-      <View style={styles.container}>
+          <View style={styles.toptop}>
+            <Text style={{textAlign: "center", fontSize: 60, fontFamily: 'Jost_500Medium'}}>Zen</Text>
+          </View>
 
-        <Text style={{textAlign: "center", fontSize: 40, fontFamily: 'Jost_500Medium'}}>Zen</Text>
+            
 
-        <Ripple style = {styles.bottom} onPress={()=>navigation.navigate("Candle")}>
-          <Text style={textButtons.normal}>Candle</Text>
-        </Ripple> 
+          <View style={styles.middle}>
+            <Image
+              style={styles.image_style}
+              source={require('../assets/green-heart.png')}
+            />
+              <Text style={{textAlign: "center", fontSize: 40, fontFamily: 'Jost_500Medium'}}>40</Text>
+              <Text style={{textAlign: "center", fontSize: 20, fontFamily: 'Jost_500Medium'}}>Heart Rate</Text>
+            
+          </View>
 
-      </View>
+          <View style={styles.middle}>
+            <Image
+                style={styles.image_style}
+                source={require('../assets/green-clock.png')}
+              />
+                <Text style={{textAlign: "center", fontSize: 40, fontFamily: 'Jost_500Medium'}}>27</Text>
+                <Text style={{textAlign: "center", fontSize: 20, fontFamily: 'Jost_500Medium'}}>Minutes meditated this week</Text>
+          </View>
+
+          
+          
+          <Ripple style = {styles.bottom} onPress={()=>navigation.navigate("Candle")}>
+            <Text style={textButtons.normal}>Begin Meditation</Text>
+          </Ripple> 
+
+          
+
+          </View>
+
+      
     );
 };
 export default Home;
